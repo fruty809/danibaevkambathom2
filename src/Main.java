aeb3a85366002f4c7749ebd4fa0aa31e5fe6758d
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-09 23:26:02 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Toyota toyota = new Toyota(2005, "Blue", "Mechanics");
+        toyota.print();
+        Lexus lexus = new Lexus(2001, "WHite", "Engine: Diesel");
+        lexus.print();
+        Mercedes mercedes = new Mercedes(2006, "Brown", "Left Side");
+        mercedes.print();
+
+        createObjects("Mercedes");
+        createObjects("Toyota");
+        createObjects("Lexus");
+
     }
+
+    public static String createObjects(String className){
+        Toyota toyota = new Toyota(2010,"Yellow","Automate");
+        Mercedes mercedes  = new Mercedes(2005,"Black","Right Side");
+        Lexus lexus = new Lexus(2015,"Yellow","Diesele");
+        if (className == "Toyota"){
+            toyota.print();
+
+        } else if (className == "Mercedes") {
+            mercedes.print();
+        } else if (className == "Lexus") {
+            lexus.print();
+        }
+        return className;
+    }
+
 }
