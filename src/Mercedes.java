13a3cd186191b0fb72f05908e4a8990c6cf767f1
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-09 23:26:26 +0600

    Update Mercedes.java

--- src/Mercedes.java
+++ src/Mercedes.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Mercedes {
+public class Mercedes extends Car {
+    private String handlebarSide;
+
+
+
+    public Mercedes(int age, String color,String handlebarSide) {
+        super(age, color);
+        this.handlebarSide = handlebarSide;
+    }
+    public String getHandlebarSide() {
+        return handlebarSide;
+
+    }
+
+
+    @Override
+    public void print() {
+        System.out.println ( " Mercedes -> " + " age: " + getAge() + " Color: " + getColor() +  " handlebarSide " + handlebarSide);
+    }
 }
