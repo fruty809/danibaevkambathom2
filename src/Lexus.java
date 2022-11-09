8979fbdc0dace54f59c3af1cce0e22884b8321a0
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-11-09 23:25:40 +0600

    Update Lexus.java

--- src/Lexus.java
+++ src/Lexus.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Lexus {
+public class Lexus extends Car{
+    private String dieselEngine;
+
+
+
+    public Lexus(int age, String color,String dieselEngine) {
+        super(age, color);
+        this.dieselEngine = dieselEngine;
+    }
+
+    public String getDieselEngine() {
+        return dieselEngine;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Lexus -> " + " age: " + getAge() + " Color: " + getColor() +  " DieselEngine: " + dieselEngine);
+    }
 }
